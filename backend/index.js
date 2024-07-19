88acf4296ab0c676c7d554b535b132c5240c47a9
Ahmed Abdelfattah <ahmedabdelfattah0022@gmail.com>
2024-07-19 04:02:26 +0300

    init files

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import morgan from 'morgan'
+
+import route from './routes/index.route.js'
+import connectDB from './config/db.config.js'
+
+const app = express()
+dotenv.config()
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+// ROUTES
+app.use('/', route)
+
+// DATABASE
+connectDB()
+
+app.listen(process.env.PORT || 8000, () => console.log('App Running...'))
